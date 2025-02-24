686fdfe1fcf1863579ab174149ead874d8a10747
yochi <yoch7376@gmail.com>
2025-02-24 09:32:13 +0200

    changes

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import{connectToDb}from "./Config/db.js";
+import { connectToDb } from "./Config/db.js";
 import UserRouter from "./Route/User.js";
 import ProductRouter from "./Route/Product.js";
 import OrderRouter from "./Route/Order.js";
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
-import {logToFileMiddleware} from "./Middlewares/LogToFile.js"
+import { logToFileMiddleware } from "./Middlewares/LogToFile.js"
 
 dotenv.config();
 connectToDb();
-const app=express();
-app.use(cors({origin:"http://127.0.0.1:5500"}))
+const app = express();
+app.use(cors({ origin: '*' }));
 app.use(express.json())
 app.use(logToFileMiddleware)
-app.use("/api/Product",ProductRouter)
-app.use("/api/User",UserRouter)
-app.use("/api/Order",OrderRouter)
-const port=process.env.PORT
-app.listen(port,()=>{
-    console.log("app is listening on port "+port)
+app.use("/api/Product", ProductRouter)
+app.use("/api/User", UserRouter)
+app.use("/api/Order", OrderRouter)
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log("app is listening on port " + port)
 })
 
 
